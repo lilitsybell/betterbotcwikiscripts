d103aaf9fd612baaea727564cd464809b9f02a7a
lilitsybell <christianwesthoff213@gmail.com>
2026-07-27 21:39:31 -0400

    Update build-scripts.js

--- build-scripts.js
+++ build-scripts.js
@@ -1,1 +1,28 @@
+const fs = require("fs");
 
+const scriptFiles = JSON.parse(
+    fs.readFileSync("scripts.json", "utf8")
+);
+
+let allScripts = [];
+
+for (const file of scriptFiles) {
+
+    const data = JSON.parse(
+        fs.readFileSync(file, "utf8")
+    );
+
+    allScripts.push(data);
+
+}
+
+
+fs.writeFileSync(
+    "scripts-data.json",
+    JSON.stringify(allScripts, null, 2)
+);
+
+
+console.log(
+    `Built scripts-data.json with ${allScripts.length} scripts`
+);
