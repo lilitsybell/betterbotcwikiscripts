f5640efc25f48cb157b55138590ab175b28586b4
lilitsybell <christianwesthoff213@gmail.com>
2026-07-28 08:54:48 -0400

    Update build-scripts.js

--- build-scripts.js
+++ build-scripts.js
@@ -1,20 +1,39 @@
 const fs = require("fs");
 
-const scriptFiles = JSON.parse(
+
+// Load script index
+const scriptList = JSON.parse(
     fs.readFileSync("scripts.json", "utf8")
 );
 
+
 let allScripts = [];
 
-for (const file of scriptFiles) {
 
+for (const entry of scriptList) {
+
+
+    // Load the actual script file
     const data = JSON.parse(
-        fs.readFileSync(file, "utf8")
+        fs.readFileSync(entry.file, "utf8")
     );
+
+
+    // Add generator metadata
+    data.push({
+
+        id: "_generator",
+
+        size: entry.size || "full"
+
+    });
+
 
     allScripts.push(data);
 
+
 }
+
 
 
 fs.writeFileSync(
